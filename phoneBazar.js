b5a9b986a6239ef646e3424623650c52a16afc80
SabbirAhmed33 <sabbir146576@gmail.com>
2022-03-02 23:01:57 +0600

    phone added dynamically

--- phoneBazar.js
+++ phoneBazar.js
@@ -15,10 +15,10 @@ const searchPhone = () => {
         div.classList.add('col');
         div.innerHTML= `
                 <div class="card h-100">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="${phone.image}" class="card-img-top" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <h5 class="card-title">${phone.phone_name}</h5>
+                      <p class="card-text">${phone.brand}</p>
                     </div>
                     <div class="card-footer">
                       <small class="text-muted">Last updated 3 mins ago</small>
